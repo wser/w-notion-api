2d9c78b81ec1a3fb95663b00ce487b9b808b11cd
wser <w.web.dev@gmail.com>
2021-10-08 14:42:35 +0200

    added express

---
+++ index.js
@@ -0,0 +1,24 @@
+// this will allow us to import our variable
+require('dotenv').config();
+// the following lines are required to initialize a Notion client
+const { Client } = require('@notionhq/client');
+// this line initializes the Notion Client using our key
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+const databaseId = process.env.NOTION_API_DATABASE;
+
+exports.getDatabase = async function () {
+  const response = await notion.databases.query({ database_id: databaseId });
+
+  const responseResults = response.results.map((page) => {
+    return {
+      id: page.id,
+      name: page.properties.Name.title[0]?.plain_text,
+      role: page.properties.Role.rich_text[0]?.plain_text,
+    };
+  });
+
+  return responseResults;
+};
+
+getDatabase();
