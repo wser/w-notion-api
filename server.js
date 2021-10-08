bb4f3b739ef09377cbd545b363625fc2b0476362
wser <w.web.dev@gmail.com>
2021-10-08 14:48:50 +0200

    modified server

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ const getDatabase = moduleToFetch.getDatabase;
 const port = 8000;
 const app = express();
 
-// this last command will log a message on your terminal when you do `npm start`
+// the code line we just added
+app.use(express.static('public'));
+
+app.get('/users', async (req, res) => {
+  const users = await getDatabase();
+  res.json(users);
+});
+
 app.listen(port, console.log(`Server started on ${port}`));
